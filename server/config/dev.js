c7c5558f194852d9594e99b37dcb5e82a21f11b7
Pavlos <pavloskoz@hotmail.com>
2019-02-08 14:12:55 -0600

    clean up git

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    client_id: "643dec238be64133954e4e27424cf5eb",
-    "client_secret": "912cdacc291042e8be99e6e652b0955a",
-    "redirect_uri": "auth_grant_callback",
-    "scope": "user-read-private user-read-email"
-}
