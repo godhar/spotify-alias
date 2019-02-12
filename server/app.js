bef870bab83b332f1e974d1401a01c2ac3ea1974
Pavlos <pavloskoz@hotmail.com>
2019-02-12 15:31:33 -0600

    some git thing

--- server/app.js
+++ server/app.js
@@ -5,56 +5,57 @@ const keys = require('./config/keys');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 
-
-
-
 const app = express();
 
 
-app.get('/login', function (req, res) {
+app.get('/', (req, res) => {
 
-  console.log('what the fuck?');
-  console.log(process.env);
+  res.send({hi: 'there'});
+  // console.log('what the fuck?');
+  // console.log(process.env);
 
-  var spotifyApi = new SpotifyWebApi({
-    clientId: keys.client_id,
-    clientSecret: keys.client_secret,
-    redirectUri: keys.redirect_uri,
-    state: keys.state
-  });
+  // var spotifyApi = new SpotifyWebApi({
+  //   clientId: keys.client_id,
+  //   clientSecret: keys.client_secret,
+  //   redirectUri: keys.redirect_uri,
+  //   state: keys.state
+  // });
 });
 
-app.get('/callback', function (req, res) {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 
-  /* Read query parameters */
-  var code = req.query.code; // Read the authorization code from the query parameters
-  var state = req.query.state; // (Optional) Read the state from the query parameter
-  console.log(req);
-  /* Get the access token! */
-  spotifyApi.authorizationCodeGrant(code)
-    .then(function (data) {
-      console.log('The token expires in ' + data['expires_in']);
-      console.log('The access token is ' + data['access_token']);
-      console.log('The refresh token is ' + data['refresh_token']);
+// app.get('/callback', (req, res) => {
 
-      /* Ok. We've got the access token!
-         Save the access token for this user somewhere so that you can use it again.
-         Cookie? Local storage?
-      */
+//   /* Read query parameters */
+//   var code = req.query.code; // Read the authorization code from the query parameters
+//   var state = req.query.state; // (Optional) Read the state from the query parameter
+//   console.log(req);
+//   /* Get the access token! */
+//   spotifyApi.authorizationCodeGrant(code)
+//     .then(function (data) {
+//       console.log('The token expires in ' + data['expires_in']);
+//       console.log('The access token is ' + data['access_token']);
+//       console.log('The refresh token is ' + data['refresh_token']);
 
-      res.cookie('spotify_token', data['access_token']);
+//       /* Ok. We've got the access token!
+//          Save the access token for this user somewhere so that you can use it again.
+//          Cookie? Local storage?
+//       */
 
-      spotifyApi.setAccessToken(data.body['access_token']);
-      spotifyApi.setRefreshToken(data.body['refresh_token']);
+//       res.cookie('spotify_token', data['access_token']);
 
-      /* Redirecting back to the main page! :-) */
-      res.redirect('/');
+//       spotifyApi.setAccessToken(data.body['access_token']);
+//       spotifyApi.setRefreshToken(data.body['refresh_token']);
 
-    }, function (err) {
-      res.status(err.code);
-      res.send(err.message);
-    });
-});
+//       /* Redirecting back to the main page! :-) */
+//       res.redirect('/');
+
+//     }, function (err) {
+//       res.status(err.code);
+//       res.send(err.message);
+//     });
+// });
 
 
-app.listen(8888);
+// app.listen(8888);
