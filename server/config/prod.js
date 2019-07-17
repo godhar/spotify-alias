74b4ecd3b4ccc32bed46c2243d784cd8804eb453
Pavlos <pavloskoz@hotmail.com>
2019-07-16 19:20:50 -0500

    Untrack files in .gitignore

--- server/config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
-    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    scope: "user-read-private user-read-email",
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
-}
