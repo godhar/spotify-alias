9e9dd4f16afe0c32fb270e924aa09d1a47102990
Pavlos <pavloskoz@hotmail.com>
2019-08-03 15:47:35 -0500

    * make adjustments for prod deploy

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === 'production') {
 
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist",));
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
     });
 }
 
