294a66f9406e2f944527beedf91a8a9a4a2e7425
Pavlos <pavloskoz@hotmail.com>
2019-08-04 16:51:50 -0500

    * make adjustments for prod deploy

--- server/app.js
+++ server/app.js
@@ -27,12 +27,11 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/spotifyDataRoutes')(app);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/dist/index.html'));
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('/client/dist/client/index.html'));
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "dist", "client", "index.html"));
     });
 }
 
